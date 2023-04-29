03b15fa5e10f7d48c4e7c1fd4d7cf245018aa2bd
Sherif Shabana <monoboly7@gmail.com>
2023-04-29 15:58:57 +0300

    Adjustments on the multimedia functionality
    Adjusted the response.
    Modified the Message model to accommodate the changes

--- database/migrations/2023_02_05_185957_create_messages_table.php
+++ database/migrations/2023_02_05_185957_create_messages_table.php
@@ -17,6 +17,7 @@ class CreateMessagesTable extends Migration
 			$table->integer('user_id');
 			$table->boolean('seen')->default(0);
 			$table->tinyInteger('type')->default(1);
+			$table->string('file')->nullable();
 		});
 	}
 
